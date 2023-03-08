bd491dc0bdce5065f9d3ddde32e0d3527a7957da
Zabi <zbvmihai@gmail.com>
2023-03-08 12:23:50 +0000

    Add Gallery Button and it's functionality along with grant access to external storage of the phone

--- app/src/main/java/com/zabava/drawapp/MainActivity.kt
+++ app/src/main/java/com/zabava/drawapp/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.zabava.drawapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -15,17 +23,47 @@ class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
-    private var ibSkin : ImageButton? = null
-    private var ibBlack : ImageButton? = null
-    private var ibRed : ImageButton? = null
-    private var ibGreen : ImageButton? = null
-    private var ibBlue : ImageButton? = null
-    private var ibYellow : ImageButton? = null
-    private var ibPurple : ImageButton? = null
-    private var ibWhite : ImageButton? = null
-    private var ibRandom : ImageButton? = null
+    private var ibSkin: ImageButton? = null
+    private var ibBlack: ImageButton? = null
+    private var ibRed: ImageButton? = null
+    private var ibGreen: ImageButton? = null
+    private var ibBlue: ImageButton? = null
+    private var ibYellow: ImageButton? = null
+    private var ibPurple: ImageButton? = null
+    private var ibWhite: ImageButton? = null
+    private var ibRandom: ImageButton? = null
 
-    private var palletButtons : ArrayList<ImageButton>? = null
+    private var palletButtons: ArrayList<ImageButton>? = null
+
+    private val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if (result.resultCode == RESULT_OK && result.data!=null){
+                val imageBackground: ImageView = findViewById(R.id.iv_background)
+                imageBackground.setImageURI(result.data?.data)
+            }
+        }
+
+    private val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if (isGranted) {
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+
+                    val pickIntent = Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+
+                } else {
+                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,10 +83,19 @@ class MainActivity : AppCompatActivity() {
         ibRandom = findViewById(R.id.ib_random)
 
         palletButtons = arrayListOf(
-            ibSkin!!,ibBlack!!,ibRed!!,ibGreen!!,ibBlue!!,ibYellow!!,ibPurple!!,ibWhite!!,ibRandom!!)
+            ibSkin!!,
+            ibBlack!!,
+            ibRed!!,
+            ibGreen!!,
+            ibBlue!!,
+            ibYellow!!,
+            ibPurple!!,
+            ibWhite!!,
+            ibRandom!!
+        )
 
-        for (button in palletButtons!!){
-            button?.setOnClickListener{
+        for (button in palletButtons!!) {
+            button?.setOnClickListener {
                 paintClicked(button)
             }
         }
@@ -57,32 +104,37 @@ class MainActivity : AppCompatActivity() {
 
         mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
         mImageButtonCurrentPaint!!.setImageDrawable(
-            ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
         )
 
-        val ibBrush : ImageButton = findViewById(R.id.ib_brush)
-        ibBrush.setOnClickListener{
+        val ibBrush: ImageButton = findViewById(R.id.ib_brush)
+        ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
+        }
+
+        val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener{
+            requestStoragePermission()
         }
 
     }
 
-    private fun showBrushSizeChooserDialog(){
+    private fun showBrushSizeChooserDialog() {
         var brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush size: ")
         val smallBtn = brushDialog.findViewById<ImageButton>(R.id.ib_small_brush)
-        smallBtn.setOnClickListener{
+        smallBtn.setOnClickListener {
             drawingView?.setSizeForBrush(10.toFloat())
             brushDialog.dismiss()
         }
         val mediumBtn = brushDialog.findViewById<ImageButton>(R.id.ib_medium_brush)
-        mediumBtn.setOnClickListener{
+        mediumBtn.setOnClickListener {
             drawingView?.setSizeForBrush(20.toFloat())
             brushDialog.dismiss()
         }
         val largeBtn = brushDialog.findViewById<ImageButton>(R.id.ib_large_brush)
-        largeBtn.setOnClickListener{
+        largeBtn.setOnClickListener {
             drawingView?.setSizeForBrush(30.toFloat())
             brushDialog.dismiss()
         }
@@ -91,20 +143,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun paintClicked(view: View) {
-        if(view!== mImageButtonCurrentPaint){
+        if (view !== mImageButtonCurrentPaint) {
             val imageButton = view as ImageButton
             val colorTag = imageButton.tag.toString()
             drawingView?.setColor(colorTag)
 
             imageButton.setImageDrawable(
-                ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
             )
             mImageButtonCurrentPaint?.setImageDrawable(
-                ContextCompat.getDrawable(this,R.drawable.pallet_normal)
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
             )
 
             mImageButtonCurrentPaint = view
 
+        }
+    }
+
+    private fun showRationalDialog(title: String, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title).setMessage(message).setPositiveButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
+    }
+
+    private fun requestStoragePermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)){
+            showRationalDialog("Kids Drawing App",
+                "Kids Drawing App need to access your storage")
+        }else{
+            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+            // TODO - Add writing external storage permission
         }
     }
 }
