9637812680b1d271d1b9c6e7ef3f145a0f832583
Zabi <zbvmihai@gmail.com>
2023-03-08 16:02:19 +0000

    Add save button and it's functionality using coroutines

--- app/src/main/java/com/zabava/drawapp/MainActivity.kt
+++ app/src/main/java/com/zabava/drawapp/MainActivity.kt
@@ -3,12 +3,17 @@ package com.zabava.drawapp
 import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.content.Intent
-import android.media.Image
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -18,6 +23,12 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -119,11 +130,22 @@ class MainActivity : AppCompatActivity() {
         val ibRedo: ImageButton = findViewById(R.id.ib_redo)
         ibRedo.setOnClickListener {
             drawingView?.onClickRedo()
-        }
 
+        }
         val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
         ibGallery.setOnClickListener {
             requestStoragePermission()
+        }
+
+        val ibSave: ImageButton = findViewById(R.id.ib_save)
+        ibSave.setOnClickListener{
+            if (isReadStorageAllowed()){
+                lifecycleScope.launch{
+                    val flDrawingView: FrameLayout = findViewById(R.id.fl_drawing_view_container)
+
+                    saveBitmapFile(getBitmapFromView(flDrawingView))
+                }
+            }
         }
 
     }
@@ -177,6 +199,12 @@ class MainActivity : AppCompatActivity() {
         builder.create().show()
     }
 
+    private fun isReadStorageAllowed() : Boolean {
+        val result = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
     private fun requestStoragePermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
@@ -188,13 +216,63 @@ class MainActivity : AppCompatActivity() {
                 "Kids Drawing App need to access your storage"
             )
         } else {
-            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
-            // TODO - Add writing external storage permission
+            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE))
+
         }
-
     }
 
-    private fun undoDraw() {
+    private fun getBitmapFromView(view: View) : Bitmap{
+        val returnedBitmap = Bitmap.createBitmap(view.width,
+            view.height,Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if(bgDrawable != null)
+            bgDrawable.draw(canvas)
+        else
+            canvas.drawColor(Color.WHITE)
 
+        view.draw(canvas)
+
+        return returnedBitmap
     }
+
+    private suspend fun saveBitmapFile(mBitmap: Bitmap?) : String{
+        var result = ""
+        withContext(Dispatchers.IO){
+            if (mBitmap != null) {
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+
+                    val f = File(externalCacheDir?.absoluteFile.toString() +
+                            File.separator + "DrawingApp_" +
+                            System.currentTimeMillis()/1000 + ".png")
+
+                    val fo = FileOutputStream(f)
+                    fo.write(bytes.toByteArray())
+                    fo.close()
+
+                    result = f.absolutePath
+
+                    runOnUiThread {
+                        if (result.isNotEmpty()){
+                            Toast.makeText(this@MainActivity,
+                                "File saved successfully: $result",
+                                Toast.LENGTH_SHORT).show()
+                        }else{
+                            Toast.makeText(this@MainActivity,
+                                "Something went wrong while saving the file",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                catch (e: java.lang.Exception){
+                    result=""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
+    }
+
 }
